f64a12756976aa0fe81b47cbf618586ca7e08149
Catherine Schmidt <hi@cath.land>
2024-03-26 01:10:27 +0000

    2024 update)

--- script.js
+++ script.js
@@ -6,29 +6,28 @@ $(document).ready(function(){
 
 	$(".right-arrow").click(function(){
 
-		var vispic = $(this).siblings(".pictures").children("img:visible");
+		var vispic = $(this).siblings(".pictures").children().filter(":visible");
 		var firstpic = $(this).siblings(".pictures").children().first();
 
 		if (vispic.is(':last-child') == true) {
 			vispic.hide();
 			firstpic.show();
 		} else {
-			vispic.next().show();
+			vispic.next().show().children().show();
 			vispic.hide();
 		}
 	});
 
 	$(".left-arrow").click(function(){
 
-
-		var vispic = $(this).siblings(".pictures").children("img:visible");
+		var vispic = $(this).siblings(".pictures").children().filter(":visible");
 		var lastpic = $(this).siblings(".pictures").children().last();
 
 		if (vispic.is(':first-child') == true) {
 			vispic.hide();
-			lastpic.show();
+			lastpic.show().children().show();
 		} else {
-			vispic.prev().show();
+			vispic.prev().show().children().show();
 			vispic.hide();
 		}
 	});
